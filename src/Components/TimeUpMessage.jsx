1bf0be351375fcbe1a70ec24f0ce7623b64b9d61
dash <daobandoso@est.utn.ac.cr>
2025-04-26 19:39:29 -0600

    Componente de mensaje de tiempo y sanitizacion de HTML
    Se crea el componente TimeUpMessage para mostrar un mensaje de tiempo y la funcion SanitizeHtml para limpiar el contenido HTML

---
+++ src/Components/TimeUpMessage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TimeUpMessage() {
+  return (
+    <div className="text-center mt-3 text-danger">
+      <p>¡Se acabó el tiempo!</p>
+    </div>
+  );
+}
+
+export default TimeUpMessage;
