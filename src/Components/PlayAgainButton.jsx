0cf3d884bc99c12eb01c8c4a079286e29fc55e27
Zuly <zurojasco@est.utn.ac.cr>
2025-04-29 19:01:48 -0600

    Cambio en CResults y CLogin
    Se modifica el css para mejorar la apariencia, se cambian colores y aspectos en general y ademas se eliman variables sin utilizar

--- src/Components/PlayAgainButton.jsx
+++ src/Components/PlayAgainButton.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function PlayAgainButton() {
   return (
     <div className="mt-3 d-grid gap-2">
-      <Link to="/" className="btn btn-primary">Jugar de nuevo</Link>
+      <Link to="/" className="btn-again">Jugar de nuevo</Link>
     </div>
   );
 }
