78d28313b19a97475417e41939b4605cd91211dc
RmFelipe24 <rimunozro@est.utn.ac.cr>
2025-05-01 15:58:37 -0600

    Eliminacion de espacios

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, OAuthProvider } from "firebase/auth";
 
-// Tu configuración de Firebase
+
 const firebaseConfig = {
   apiKey: "AIzaSyDbOurgKaUJ8-VSLQGi8R98p3l6Cp4ofwk",
   authDomain: "logintrivia1.firebaseapp.com",
@@ -11,7 +11,7 @@ const firebaseConfig = {
   appId: "1:681575019822:web:d6d9be49d4446b869bb6bf"
 };
 
-// Inicialización
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
